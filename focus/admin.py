8563f17ba4db2ac1c382e1bf7f6ee974d88c5a71
tangyr <tangyr1984@gmail.com>
2016-12-09 19:32:57 +0800

    test

--- focus/admin.py
+++ focus/admin.py
@@ -3,28 +3,30 @@ from django.db import models
 from django import forms
 from .models import Comment, Article, Column, NewUser,Author
 
+
+
 # Register your models here.
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user_id','article_id','pub_date', 'content','poll_num')  
- 
+    list_display = ('user_id','article_id','pub_date', 'content','poll_num')
+
 class ArticleAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.TextField: {'widget': forms.Textarea(
                            attrs={'rows': 41,
                                   'cols': 100
-                                  })}, 
+                                  })},
     }
-    list_display = ('title','pub_date', 'poll_num') 
- 
+    list_display = ('title','pub_date', 'poll_num')
+
 class NewUserAdmin(admin.ModelAdmin):
-    list_display = ('username','date_joined', 'profile') 
- 
+    list_display = ('username','date_joined', 'profile')
+
 class ColumnAdmin(admin.ModelAdmin):
-    list_display = ('name', 'intro') 
- 
+    list_display = ('name', 'intro')
+
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'profile') 
- 
+    list_display = ('name', 'profile')
+
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Column, ColumnAdmin)
